c4187d573eb60e4ae6b1a00d2ab8f4009ad57b9d
JohnCorby <williamforgames6@gmail.com>
2026-02-26 22:48:16 -0700

    ugh. syncing comments

--- QSB2/QObject/IStartable.cs
+++ QSB2/QObject/IStartable.cs
@@ -5,6 +5,8 @@ using QSB2.WakeUpSync;
 
 namespace QSB2.QObject;
 
+// TODO: this means theres a difference between "qobject exists" and "qobject is initialized (started)". its probably worth having another global flag to wait for
+//       alternatively, we just do qsb1 style make create async and then again have a qobject exist vs qobject init thing...
 public interface IStartable
 {
     public void Start();
