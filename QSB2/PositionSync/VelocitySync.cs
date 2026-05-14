45436bd8b8a485f56ea1c3155f5ef38165246e84
JohnCorby <williamforgames6@gmail.com>
2026-05-13 22:48:53 -0500

    more ordering

--- QSB2/PositionSync/VelocitySync.cs
+++ QSB2/PositionSync/VelocitySync.cs
@@ -10,16 +10,17 @@ public class VelocitySync(QObject.QObject qObject)
 {
     public Vector3 RelVel;
     public Vector3 RelAngVel;
+    public float PrevTime; // for dropping out of order messages
 
     public float UpdateInterval = 0f;
     private float _timer;
-    
+
     public void Tick()
     {
         if (qObject.Owner.ID == -1) return; // no owner = do nothing
-        
+
         if (qObject.PositionSync.Reference == null) return;
-        
+
         var refBody = qObject.PositionSync.Reference.GetAttachedOWRigidbody();
         var body = qObject.Component.GetAttachedOWRigidbody();
 
@@ -28,7 +29,7 @@ public class VelocitySync(QObject.QObject qObject)
             _timer += Time.unscaledDeltaTime;
             if (_timer < UpdateInterval) return;
             _timer = 0;
-            
+
             // owner - sync from unity component
             RelVel = refBody.ToRelVel(body.GetVelocity(), body.GetPosition());
             RelAngVel = refBody.ToRelAngVel(body.GetAngularVelocity());
@@ -36,7 +37,8 @@ public class VelocitySync(QObject.QObject qObject)
             qObject.Send(new VelocityMessage
             {
                 RelVel = RelVel,
-                RelAngVel = RelAngVel
+                RelAngVel = RelAngVel,
+                Time = Time.unscaledTime
             }, -2, Channels.Unreliable);
         }
         else
@@ -65,10 +67,13 @@ public class VelocityMessage : QObjectMessage
 {
     [Key(2)] public required Vector3 RelVel;
     [Key(3)] public required Vector3 RelAngVel;
+    [Key(4)] public required float Time;
 
     public override void OnReceive(QObject.QObject qObject, int from, int to)
     {
+        if (Time < qObject.PositionSync.PrevTime) return;
         qObject.VelocitySync.RelVel = RelVel;
         qObject.VelocitySync.RelAngVel = RelAngVel;
+        qObject.PositionSync.PrevTime = Time;
     }
 }
