d0e8f0b99343045d4d3e68206b2b63b2db009bbb
JohnCorby <williamforgames6@gmail.com>
2026-08-15 13:45:12 -0500

    Revert "old work that im committing for backup"
    This reverts commit 8e8ec90cceb585f9bc502a02bc2aedecf031ca1f.

--- QSB2/Ownership/Owner.cs
+++ QSB2/Ownership/Owner.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using MessagePack;
-using OWML.Utils;
 using QSB2.QObject;
 
 namespace QSB2.Ownership;
@@ -14,33 +12,21 @@ public class Owner(QObject.QObject qObject)
     public int ID = -1;
 }
 
-// TODO: on player leave, remove them if they are the owner and tell others
-
-// BUG: if both players force, we'll get 2 responses where it'll be one player and then the other, instead of waiting.
-//      only one should actually get it, but that means a force can fail for someone
-
 public class OwnerQueue(QObject.QObject qObject)
 {
-    public List<int> IDs;
-    public Action OnOwnerChange;
-    public bool WaitingOnResponse;
+    // BUG: without loopback, steam preserves order between clients. with loopback, these become desynced!
+    public readonly List<int> IDs = new();
 
     public void DoAction(OwnerQueueAction action, int id = -1)
     {
-        if (NetworkManager.IsHost) IDs ??= new();
-
-        WaitingOnResponse = true;
         qObject.Send(new OwnerQueueMessage
         {
             PlayerID = id == -1 ? NetworkManager.LocalID : id,
             Action = action
-        }, NetworkManager.ConnectionIDs[0]);
+        }, -1);
     }
 }
 
-/// <summary>
-/// send action to server
-/// </summary>
 [MessagePackObject]
 public class OwnerQueueMessage : QObjectMessage
 {
@@ -67,28 +53,8 @@ public class OwnerQueueMessage : QObjectMessage
                 break;
         }
 
-        qObject.Send(new OwnerMessage
-        {
-            // empty queue = no one owns
-            OwnerID = ownerQueue.Count != 0 ? ownerQueue[0] : -1
-        }, -1);
-    }
-}
-
-/// <summary>
-/// server responds with new owner
-/// </summary>
-[MessagePackObject]
-public class OwnerMessage : QObjectMessage
-{
-    [Key(2)]
-    public required int OwnerID;
-
-    public override void OnReceive(QObject.QObject qObject, int from, int to)
-    {
-        qObject.Owner.ID = OwnerID;
-        qObject.OwnerQueue.WaitingOnResponse = false;
-        qObject.OwnerQueue.OnOwnerChange?.SafeInvoke();
+        // empty queue = no one owns
+        qObject.Owner.ID = ownerQueue.Count != 0 ? ownerQueue[0] : -1;
     }
 }
 
