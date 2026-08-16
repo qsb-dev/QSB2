d2e6887509e5553b43eeb761af04a062073bf3cc
JohnCorby <williamforgames6@gmail.com>
2026-08-16 17:43:35 -0500

    fix race condition with owner queue

--- QSB2/Ownership/Owner.cs
+++ QSB2/Ownership/Owner.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
 using MessagePack;
-using QSB2.Messaging;
+using OWML.Utils;
 using QSB2.QObject;
 
 namespace QSB2.Ownership;
@@ -13,29 +13,37 @@ public class Owner(QObject.QObject qObject)
     public int ID = -1;
 }
 
+// TODO: on player leave, remove them if they are the owner and tell others
+
 public class OwnerQueue(QObject.QObject qObject)
 {
-    // BUG: race condition if 2 things try to add. will be different order. especially true for forcing, which can fail
-    public readonly List<int> IDs = new();
+    public List<int> IDs;
+    public Action OnOwnerChange;
+    public bool WaitingOnResponse;
 
     public void DoAction(OwnerQueueAction action, int id = -1)
     {
-        qObject.Send(new OwnerQueueMessage
+        WaitingOnResponse = true;
+        qObject.Send(new OwnerActionMessage
         {
             PlayerID = id == -1 ? NetworkManager.LocalID : id,
             Action = action
-        }, SendTo.All);
+        }, NetworkManager.ConnectionIDs[0]);
     }
 }
 
+/// <summary>
+/// send action to server
+/// </summary>
 [MessagePackObject]
-public class OwnerQueueMessage : QObjectMessage
+public class OwnerActionMessage : QObjectMessage
 {
     [Key(2)] public required int PlayerID;
     [Key(3)] public required OwnerQueueAction Action;
 
     public override void OnReceive(QObject.QObject qObject, int from, int to)
     {
+        qObject.OwnerQueue.IDs ??= new();
         var ownerQueue = qObject.OwnerQueue.IDs;
 
         switch (Action)
@@ -54,8 +62,27 @@ public class OwnerQueueMessage : QObjectMessage
                 break;
         }
 
-        // empty queue = no one owns
-        qObject.Owner.ID = ownerQueue.Count != 0 ? ownerQueue[0] : -1;
+        qObject.Send(new OwnerMessage
+        {
+            // empty queue = no one owns
+            OwnerID = ownerQueue.Count != 0 ? ownerQueue[0] : -1
+        }, -1);
+    }
+}
+
+/// <summary>
+/// server responds with new owner
+/// </summary>
+[MessagePackObject]
+public class OwnerMessage : QObjectMessage
+{
+    [Key(2)] public required int OwnerID;
+
+    public override void OnReceive(QObject.QObject qObject, int from, int to)
+    {
+        qObject.Owner.ID = OwnerID;
+        qObject.OwnerQueue.WaitingOnResponse = false;
+        qObject.OwnerQueue.OnOwnerChange?.SafeInvoke();
     }
 }
 
@@ -72,7 +99,8 @@ public enum OwnerQueueAction : byte
     Remove,
 
     /// <summary>
-    /// add player to the queue and force them to the front
+    /// add player to the queue and force them to the front.
+    /// use with caution, as someone else may steal your ownership at any time, including immediately after you force.
     /// </summary>
     Force
 }
