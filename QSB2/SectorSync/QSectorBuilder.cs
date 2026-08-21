e643cf7676e64742d02d456d184ef0a562e73bcb
JohnCorby <williamforgames6@gmail.com>
2026-08-21 18:20:41 -0500

    oops

--- QSB2/SectorSync/QSectorBuilder.cs
+++ QSB2/SectorSync/QSectorBuilder.cs
@@ -155,7 +155,7 @@ public class FakeSector : Sector
         go2.transform.SetParent(go.transform, false);
 
         var fakeSector = go2.AddComponent<FakeSector>();
-        fakeSector._name = (Name)(SendTo.All);
+        fakeSector._name = (Name)(-1);
         fakeSector._subsectors = new List<Sector>();
         fakeSector._idString = name;
         fakeSector.SetParentSector(parent);
